c7d33cb419022f053ba3f55b2917e078096c224b
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-06-17 23:55:35 +0700

    Add in feature to display during testing

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import cv2
 import time
+import config
+import imutils
+import sqlite3
 import logging
 import traceback
 import numpy as np
+import pandas as pd
 from pathlib import Path
 from logging.handlers import RotatingFileHandler
 
@@ -44,6 +49,70 @@ def rank5_accuracy(preds, labels):
     return (round(rank1, 2), round(rank5, 2))
 
 
+def read_list_file(list_file_path):
+    data = pd.read_csv(list_file_path, sep="\t", header=None)
+    data.columns = ["lst_idx", "label_id", "img_path"]
+    return data
+
+
+def get_img_path(lst_idx, lst_data):
+    return lst_data[lst_data["lst_idx"] == lst_idx]["img_path"].values[0]
+
+
+def get_label_name(label_idx):
+    query = f"""
+    SELECT make || ' : ' || model AS make_model
+    FROM make_model
+    WHERE make_model_id = {label_idx}
+    """
+    label_df = pd.DataFrame()
+    with sqlite3.connect(config.SQLITE3_DB_FILE) as conn:
+        label_df = pd.read_sql_query(query, conn)
+
+    return label_df["make_model"].values[0]
+
+
+def img_show_rankN(preds, labels, list_file_path, N=5):
+    lst_data = read_list_file(list_file_path)
+    lst_idx = 0
+    for (pred, act_label_idx) in zip(preds, labels):
+        pred_sorted_idx = np.argsort(pred)[::-1]
+
+        # Show highest score image
+        pred_label_idx = pred_sorted_idx[0]
+        img_path = get_img_path(lst_idx, lst_data)
+        pred_score = round(pred[pred_label_idx]*100, 2)
+
+        pred_label = get_label_name(pred_label_idx)
+        act_label = get_label_name(act_label_idx)
+
+        print(
+            f"*** Predicted: {pred_label:>30} (conf.={pred_score}%) | Actual: {act_label}")
+
+        # Prepare for display the image
+        image = cv2.imread(img_path)
+        orig = image.copy()
+        orig = imutils.resize(orig, width=min(400, orig.shape[1]))
+
+        img_label = pred_label.replace(": ", " ")
+        img_label = f"{img_label} : {pred_score} %"
+        cv2.putText(orig, img_label, (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        # Print out the result of top N images' label
+        for i in range(N-1):
+            iter_label_idx = pred_sorted_idx[i+1]
+            iter_pred_score = round(pred[iter_label_idx]*100, 2)
+            iter_pred_label = get_label_name(iter_label_idx)
+            print(
+                f"\tPredicted: {iter_pred_label:>30} | confident score ={iter_pred_score:>15}%")
+
+        lst_idx += 1
+        # show the image
+        cv2.imshow("Image", orig)
+        cv2.waitKey(0)
+
+
 def timeit(method):
     """
     The util to calculate the time consuming of each function using decorator
@@ -66,3 +135,8 @@ def exception_to_string(ex):
     This is used for debugging purpose by logging the error detail into log file
     """
     return ''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__))
+
+
+def row_count(file):
+    with open(file, 'r') as f:
+        return sum(1 for _ in f)
